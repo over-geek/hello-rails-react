f0aa26825250b3b9dd5acf53e58c98ab166519e2
over-geek <kenagy100@gmail.com>
2024-02-20 02:07:24 +0000

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  root 'root#index'
+  namespace :api do
+    get 'greetings/random_greeting'
+  end
+   root 'root#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
