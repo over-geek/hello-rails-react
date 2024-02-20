00aaeb6b14ca8e90a9a23c98ace0476e44e601ce
over-geek <kenagy100@gmail.com>
2024-02-20 02:23:27 +0000

    refactor component

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,15 +1,19 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter } from 'react-router-dom'
+import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
-function App() {
-  return (<h1>H!</h1>);
-}
-
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
